cc93aca0d4cbe2d1aac9c0e2be94026449534053
Sonia Foco <sonia.foco02@gmail.com>
2026-01-29 08:27:09 +0100

    fix

--- QA/code/qwen-3b.py
+++ QA/code/qwen-3b.py
@@ -20,20 +20,19 @@ def main():
             device_map="auto",
         )
     parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str) 
     parser.add_argument("--output_path", type=str) 
-    parser.add_argument("--sentence_type", type=str) # "src" or "bt_tgt"
-    parser.add_argument("--prompt", type=str) #atomic/vanilla
+    parser.add_argument("--sentence_type", type=str) #src/bt_tgt
 
     args = parser.parse_args()
 
     # =========================================== Load Dataset ===========================================
-    with open(f"../QG/{args.prompt}_qwen-3B.jsonl", 'r') as f_qg, open(f"askqe_atomic_facts_bt.jsonl", 'r') as f_bt, open(f"{args.output_path}-{args.prompt}.jsonl", 'a') as f_out:
-        for qg_line, bt_line in zip(f_qg, f_bt):
-            qg_data = json.loads(qg_line)
-            data = json.loads(bt_line)
+    with open(args.input_path, 'r') as f_in, open(args.output_path, 'a') as f_out:
+        for line in f_in:
+            data = json.loads(line)
 
-            questions = qg_data.get("questions")
             sentence = data.get(args.sentence_type, None)
+            questions = data.get("questions", None)
 
             if sentence and questions:
                 prompt_template = qa_prompt
