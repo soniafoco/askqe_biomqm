d17526a762cb3b26729012f3d81bc4057161087b
Sonia Foco <sonia.foco02@gmail.com>
2026-02-10 20:02:04 +0100

    added nli metric

--- extension_nli_metric/nli_metric.py
+++ extension_nli_metric/nli_metric.py
@@ -8,127 +8,38 @@ from tqdm import tqdm
 import re
 import ast
 
+ENTAILMENT_SCORE = 1
+CONTRADICTION_SCORE = 0
+NEUTRAL_SCORE = 0.5
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description="NLI-based scoring of answers using entailment"
     )
-    parser.add_argument("--input_src", type=str, required=True,
-                        help="Input JSONL file with answers")
-    parser.add_argument("--input_bt", type=str, required=True,
-                        help="Input JSONL file with answers")
+    parser.add_argument("--input", type=str, required=True,
+                        help="Input JSONL file with questions, answers (SRC and BT) and sbert score")
     parser.add_argument("--output", type=str, required=True,
                         help="Output JSONL file with entailed answers")
-    parser.add_argument("--threshold", type=float, default=0.5,
-                        help="Entailment probability threshold")
     return parser.parse_args()
 
-def normalize(text):
-    text = text.lower()
-    text = re.sub(r"[^a-z0-9\s]", " ", text)
-    text = re.sub(r"\s+", " ", text).strip()
-    return text
-
-def lexical_grounded(answer, source, min_overlap=2):
-    """
-    Returns True if answer shares at least `min_overlap`
-    content tokens with the source.
-    """
-    ans_tokens = set(normalize(answer).split())
-    src_tokens = set(normalize(source).split())
-
-    stopwords = {"the", "a", "an", "of", "to", "and", "is", "are"}
-    ans_tokens = ans_tokens - stopwords
-
-    overlap = ans_tokens & src_tokens
-    return len(overlap) >= min_overlap
 
 
-def is_entailed_sentence(source, answer, tokenizer, model, device, threshold):
-    inputs = tokenizer(
-        source,
-        answer,
-        truncation=True,
-        max_length=512,
-        return_tensors="pt"
-    ).to(device)
+def get_nli_label(model, question, pred, ref):
+    premise = question + ' ' + ref + '.'
+    hypothesis = question + ' ' + pred + '.'
 
-    with torch.no_grad():
-        logits = model(**inputs).logits
-
-    probs = F.softmax(logits, dim=-1)
-    entailment_score = probs[0][2].item()  # entailment
-
-    return entailment_score >= threshold
-
-
-def supported_in_text(source, answer, tokenizer, model, device, threshold=0.5):
-    """
-    Returns True ONLY if the answer introduces supported content.
-    """
-
-    # STEP 1: lexical grounding
-    if lexical_grounded(answer, source):
-        return True  
-
-    # STEP 2: NLI (solo se lessicalmente non grounded)
-    wrapped_answer = f"The text mentions {answer} or a synonim."
-    entailed = is_entailed_sentence(
-        source, wrapped_answer, tokenizer, model, device, threshold
+    res = model.predict(
+        premise=premise,
+        hypothesis=hypothesis
     )
 
-    return entailed
-
-
-def parse_maybe_list(x):
-    """
-    Handles:
-    - real lists
-    - JSON strings
-    - broken strings like: [687, 1514, number of participants, 581]
-    """
-    if isinstance(x, list):
-        return x
-
-    if isinstance(x, str):
-        # try JSON
-        try:
-            v = json.loads(x)
-            if isinstance(v, list):
-                return v 
-            else:
-                print(x)
-                return []
-        except Exception:
-            pass
-        
-        try:
-            v = list([str(x)])
-            if isinstance(v, list):
-                return v 
-            else:
-                print(x)
-                return []
-        except Exception:
-            pass
-
-        # try python-like list
-        try:
-            v = ast.literal_eval(x)
-            if isinstance(v, list):
-                return v 
-            else:
-                print(x)
-                return []
-        except Exception:
-            return []
-
-    return []
+    return res['label']
 
 
 def main():
     args = parse_args()
 
-    nli_model_name = "cross-encoder/nli-deberta-v3-large"
+    nli_model_name = "roberta-large-mnli"
     os.makedirs(os.path.dirname(args.output), exist_ok=True)
 
     tokenizer = AutoTokenizer.from_pretrained(nli_model_name)
@@ -140,69 +51,87 @@ def main():
 
     print("NLI model loaded on", device)
 
-    with open(args.input, "r", encoding="utf-8") as fin, \
-         open(args.output, "w", encoding="utf-8") as fout:
-
-        for line_num, line in enumerate(tqdm(fin), start=1):
-            try:
-                obj = json.loads(line)
-            except json.JSONDecodeError as e:
-                print(f"JSON error on line {line_num}: {e}")
-                continue
-
-            if args.sentence_type == "src":
-                answers = obj.get("answers", [])
-            elif args.sentence_type == "bt_tgt":
-                answers = obj.get("answers_bt", [])
-            source = obj.get(args.sentence_type, []) #src or bt_tgt
-            questions = obj.get("questions", [])
-
-            if not source:
-                continue
-
-            answers = parse_maybe_list(answers)
-            questions = parse_maybe_list(questions)
-
-            entailed_answers = []
-            entailed_questions = []
-
-            for ans, q in zip(answers, questions):
-                print(q)
-                print(ans)
-                if str(ans).lower() =="yes" or str(ans).lower() =="no" or str(ans).lower() =="not specified":
-                    entailed_answers.append(ans)
-                    entailed_questions.append(q)
-                    continue
+    try:
+        with open(args.input, "r", encoding="utf-8") as f_in, open(args.output, "w", encoding="utf-8") as out_f:
+            for line in f_in:
                 try:
-                    if supported_in_text(str(ans), source, tokenizer, model, device, args.threshold):
-                        entailed_answers.append(ans)
-                        entailed_questions.append(q)
+                    data = json.loads(line)
+
+                    predicted_answers = data.get("answers_bt", [])
+                    reference_answers = data.get("answers_src", [])
+                    questions = data.get("questions", [])
+                    scores = data.get("scores", [])
+
+                    if isinstance(predicted_answers, str):
+                        try:
+                            predicted_answers = json.loads(predicted_answers)
+                        except json.JSONDecodeError:
+                            continue
+
+                    if isinstance(reference_answers, str):
+                        try:
+                            reference_answers = json.loads(reference_answers)
+                        except json.JSONDecodeError:
+                            continue
+
+                    if isinstance(questions, str):
+                        try:
+                            questions = json.loads(questions)
+                        except json.JSONDecodeError:
+                            continue
+
+                    if not isinstance(predicted_answers, list) or not isinstance(reference_answers, list) or not isinstance(questions, list):
+                        continue
+                    if not predicted_answers or not reference_answers or len(predicted_answers) != len(reference_answers) != len(questions):
+                        continue
+
+                    nli_scores = []
+
+                    for pred, ref, question, score in zip(predicted_answers, reference_answers, questions, scores):
+                        """
+                        if not isinstance(pred, str) or not isinstance(ref, str):
+                            continue
+                        if pred.strip() == "" or ref.strip() == "":
+                            continue
+                        """
+                        f1_score = score["f1"]
+                        if f1_score is None:
+                            continue
+
+                        # If there is no answer - can't run NLI, keep the score 0
+                        if str(pred).lower() != "no answer" and str(ref).lower() == "no answer":
+                            nli_score = 0
+
+                        # If the score is 1, there is a full overlap between the
+                        # candidate and the predicted answer, so the score is 1
+                        if 0 <= f1_score < 1:
+                            nli_label = get_nli_label(model, str(question), str(pred), str(ref))
+
+                            if nli_label == 'entailment':  # If entails, the score is 1
+                                nli_score = ENTAILMENT_SCORE
+                            elif nli_label == 'contradiction':  # If contradicts, the score is 0
+                                nli_score = CONTRADICTION_SCORE
+                            elif nli_label == 'neutral':
+                                nli_score = f1_score
+
+                        nli_scores.append(nli_score)
+                            
+
+                    data["nli_scores"] = nli_scores
+                    if len(nli_scores) == 0:
+                        data["avg_nli"] = None
                     else:
-                        print("\nNOT SUPPORTED:")
-                        print("ANSWER:", ans)
-                        print("SOURCE:", source)
+                        data["avg_nli"] = sum(nli_scores) / len(nli_scores)
+                
+                    out_f.write(json.dumps(data, ensure_ascii=False) + "\n")
 
-                except Exception as e:
-                    print("Error", str(ans)[:50], e)
+                except json.JSONDecodeError as e:
+                    print(f"Skipping a corrupted line due to JSONDecodeError: {e}")
+                    continue
 
-            # FORCE JSON STRING REPRESENTATION
-            obj["answers"] = json.dumps(
-                [str(a) for a in entailed_answers],
-                ensure_ascii=False
-            )
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
 
-            obj["questions"] = json.dumps(
-                [str(q) for q in entailed_questions],
-                ensure_ascii=False
-            )
-
-            fout.write(json.dumps(obj, ensure_ascii=False) + "\n")
-            fout.flush()
-
-            print(len(entailed_answers), "/", len(answers), "entailed")
-
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     main()
