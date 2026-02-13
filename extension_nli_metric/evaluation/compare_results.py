30e271975e8fb4c5697cbaa6e47912df6ad7ed47
Sonia Foco <sonia.foco02@gmail.com>
2026-02-13 21:34:30 +0100

    update

--- extension_nli_metric/evaluation/compare_results.py
+++ extension_nli_metric/evaluation/compare_results.py
@@ -4,11 +4,11 @@ import pandas as pd
 import numpy as np
 import argparse
 
-def compare(input_file, output_file): 
+def compare(input_file, output_grouped, output_global): 
     results = []
 
     with open(input_file, "r", encoding="utf-8") as f_nli:
-        for line in input_file:
+        for line in f_nli:
             try:
                 data = json.loads(line)
 
@@ -27,16 +27,26 @@ def compare(input_file, output_file):
 
     df = pd.DataFrame(results)
 
+    # Summary per langguage and error type
     df['Severity'] = df['Severity'].apply(lambda x: 'Major+Critical' if x in ['Major', 'Critical'] else x)
-
     summary = df.groupby(["Language", "Severity"]).mean()
-    summary.to_csv(output_file, index=True, encoding="utf-8")
+    summary.to_csv(output_grouped, index=True, encoding="utf-8")
+    print("GROUPED (lang-error) summary:")
+    print(summary)
+
+    # Global summary
+    numeric_columns = ["F1", "EM", "CHRF", "BLEU", "SBERT"]
+    global_summary = df[numeric_columns].mean()
+    global_summary.to_csv(output_global, index=True, encoding="utf-8")
+    print("GLOBAL summary:")
+    print(global_summary)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, required=True)
-    parser.add_argument('--output', type=str, required=True)
+    parser.add_argument('--output_grouped', type=str, required=True)
+    parser.add_argument('--output_global', type=str, required=True)
     
     args = parser.parse_args()
     
-    compare(args.input, args.output)
+    compare(args.input, args.output_grouped, args.output_global)
